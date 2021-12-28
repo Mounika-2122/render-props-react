1c875f4ef74b8801ec33819eb6368f665df21756
Mounika <bojjamounika8@gmail.com>
2021-12-28 23:29:43 +0530

    render

---
+++ src/App.js
@@ -0,0 +1,51 @@
+//import logo from './logo.svg';
+//import './App.css';
+
+//function App() {
+//  return (
+  //  <div className="App">
+    //  <header className="App-header">
+      //  <img src={logo} className="App-logo" alt="logo" />
+       // <p>
+        //  Edit <code>src/App.js</code> and save to reload.
+        //</p>
+        //<a
+          //className="App-link"
+          //href="https://reactjs.org"
+          //target="_blank"
+          //rel="noopener noreferrer"
+        //>
+          //Learn React
+        //</a>
+      //</header>
+    //</div>
+  //);
+//}
+
+//export default App;
+
+import React from "react";
+class MouseTracker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.state = { x: 0, y: 0 };
+  }
+
+  handleMouseMove(event) {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY
+    });
+  }
+
+  render() {
+    return (
+      <div style={{ height: '100vh' }} onMouseMove={this.handleMouseMove}>
+        <h1>Move the mouse around!</h1>
+        <p>The current mouse position is ({this.state.x}, {this.state.y})</p>
+      </div>
+    );
+  }
+}
+export default MouseTracker;
